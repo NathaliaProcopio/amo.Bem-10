5933b205cc40811bd9400cf1a1a757af02eeee58
NathaliaProcopio <nathaliaprocopio09@gmail.com>
2025-09-09 14:19:03 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st 
+
+st.title("Meus projetos 🏹💘")
+st.caption("Criado com streamlit 💌")
+
+st.caption("Pretendente surpresa 💗")
+st.image("https://img.olhardigital.com.br/wp-content/uploads/2023/04/pretendente-surpresa-k-drama-e-dorama-da-netflix.png")
+
+st.markdown(
+"""
+### Doramas que indico
+"""
+)
+
+st.title("Bon Appétit, Vossa Majestade")
+st.caption("Fala sobre uma master chef que viajou no tempo, e que encantou o rei com sua culinária moderna 💞👩🏻‍🍳")
+st.image("https://imagens.ne10.uol.com.br/veiculos/_midias/jpg/2025/09/01/1224x674/1_bon_appetit__vossa_majestade-35468944.jpg")
+
+st.title("Além do direito 📚💼")
+st.caption("Uma jovem advogada com um forte senso de justiça entra em um escritório importante e encara as complexidades do mundo jurídico com a ajuda de um mentor frio e exigente.")
+st.image("https://br.web.img2.acsta.net/c_300_300/img/e8/c3/e8c37eaf5724d45a51de35d971217396.jpg")
+
+
